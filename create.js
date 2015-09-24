261aab0e9b549e661c48849e1e77a9f3815ebfaf
eoin-mc-carthy <em1081@my.bristol.ac.uk>
2015-09-24 19:17:07 +0100

    made pge scrollable after leaving splash

--- create.js
+++ create.js
@@ -14,10 +14,13 @@ function getTags(url) {
 }
 
 function createContent() {
+
   document.getElementById('splash').addEventListener('click', function() {
     document.getElementById('splash').style.bottom = '100vh';
+
     document.getElementsByClassName('wrapper')[0].style["margin-top"] = 0;
     document.getElementsByClassName('wrapper')[0].style["display"] = "block"
+    document.getElementsByTagName('body')[0].style["overflow-y"] = "auto";
   });
   var url = "http://content.guardianapis.com/search?section=music&api-key=test&show-fields=all&show-most-viewed=true";
   var request = new XMLHttpRequest();
