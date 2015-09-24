fa5db2fecc369880b59fbe4f16f66af505363a51
Jack Carlisle <jackcarlisle@hotmail.co.uk>
2015-09-24 10:56:02 +0100

    make queries more robust

--- main.js
+++ main.js
@@ -1,16 +1,47 @@
-function generateURL(tags_array) {
-  var url_temp = "http://api.soundcloud.com/search?q=" + tags_array[0] + "%20" + tags_array[1] + "%20" + tags_array[2]+ "&client_id=a2b378fe2c15350323249b3ab162e4bf";
-  var url_api = url_temp.replace(/ /g, "%20");
-  // creates Soundcloud API link
+var temp;
 
+function createSoundcloudQuery(tags) {
+  var out = "http://api.soundcloud.com/search?q=";
+  for(var j = 0; j < tags.length && j < 2; j++) {
+    out += tags[j]
+    if(j !== tags.length - 1){
+      out += "%20";
+    }
+
+  }
+  out += "&client_id=a2b378fe2c15350323249b3ab162e4bf";
+  return out.replace(/ /g, "%20");
+}
+
+var j=0;
+function generateURL(tags_array) {
+  j++;
+  if (j==4)
+    console.log("!");
+
+  console.log(tags_array);
+  var type, songUrl, i;
+  //var url_temp = "http://api.soundcloud.com/search?q=" + tags_array[0] + "%20" + tags_array[1] + "%20" + tags_array[2]+ "&client_id=a2b378fe2c15350323249b3ab162e4bf";
+  var url_api = createSoundcloudQuery(tags_array);
+  // creates Soundcloud API link
+  console.log(url_api);
   var request2 = new XMLHttpRequest();
   request2.open("GET", url_api, false);
   request2.send(null);
   var data2 = JSON.parse(request2.responseText);
+  i = 0;
   var arraySongURIs = data2.collection.map(mappingsongs);
-  // gets soundcloud API URIs and puts them in an array
+  do {
+    type = data2.collection[i].kind;
+    songUrl = data2.collection[i].uri;
+    i++;
+  } while(type !== "track")
 
-  return "https://w.soundcloud.com/player/?url=" + arraySongURIs[0] + "&color=0066cc"; // generates the url for soundcloud player
+  // gets soundcloud API URIs and puts them in an array
+  temp = data2;
+  console.log(songUrl);
+
+  return "https://w.soundcloud.com/player/?url=" + songUrl + "&color=0066cc"; // generates the url for soundcloud player
 
 }
 
